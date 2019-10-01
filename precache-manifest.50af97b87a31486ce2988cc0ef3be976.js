dee94a4a695401e887a24c44afda95fe07290be3
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-10-01 16:16:11 +0530

    Updates

---
+++ precache-manifest.50af97b87a31486ce2988cc0ef3be976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5ef8360a191353a6db50b8414dacf5",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/index.html"
+  },
+  {
+    "revision": "113741223d3f96070956",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/static/css/main.d1171dd6.chunk.css"
+  },
+  {
+    "revision": "ccaac33636908fa96cf7",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/static/js/2.0bbedd42.chunk.js"
+  },
+  {
+    "revision": "113741223d3f96070956",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/static/js/main.268aec81.chunk.js"
+  },
+  {
+    "revision": "991bca46c9cce16c3b91",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/static/js/runtime~main.a8170f0e.js"
+  },
+  {
+    "revision": "e2ddf1c608a75add459e67ce3da4cc76",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/static/media/landinpage.e2ddf1c6.png"
+  },
+  {
+    "revision": "9858f6aa2e07b94ec3cf8f9d547892ed",
+    "url": "/shubhamkakkar/dsc-mait-react-frontend.git/static/media/teach.9858f6aa.png"
+  }
+]);
